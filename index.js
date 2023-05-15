087ac19cf36163cf9b511d563fa76e733f7a860b
Valen Cesca <valentinacesca.utn@gmail.com>
2023-05-15 20:40:24 -0300

    navbar por usuario

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ if(obtenerUsuarioLogeado){
     usuarioLogeado = JSON.parse(obtenerUsuarioLogeado) || [];
 }
 
+
 /*1) Admin --> aparece admin en navbar y cerrar sesion --- desaparecen login y registro
 2) usuario comun --> cerrar sesion --- desaparecen login y registro
 3) sin logeo --> fav pero te lleva login --> login y reg aparecen*/
@@ -13,8 +14,9 @@ const linkRegistro = document.getElementById("linkRegistro");
 const btnCerrarSesion = document.getElementById("btnCerrarSesion");
 const btnIniciarSesion = document.getElementById("btnIniciarSesion");
 
+console.log(usuarioLogeado.userLogin);
 /* caso 3*/
-if(usuarioLogeado.user===`none`){
+if(usuarioLogeado.userLogin==`none`){
     /*caso sin logearse*/
     linkAdmin.className ="d-none";
     btnCerrarSesion.className= "d-none";
@@ -23,10 +25,18 @@ if(usuarioLogeado.user===`none`){
     /*caso de logeo sin admin*/
     linkRegistro.className ="d-none";
     btnIniciarSesion.className = "d-none";
-    if(usuarioLogeado.user===`admin`){
+    if(usuarioLogeado.userLogin==`admin`){
         /*caso admin */
 
     }else{
     linkAdmin.className ="d-none";
     }
 } 
+
+/*btnCerrarSesion.addEventListener("click", (e) => {
+    if (usuarioLogeado !==`none`) {
+        usuarioLogeado = [`none`]
+        localStorage.setItem("userLogin", JSON.stringify(usuarioLogeado));
+        localStorage.removeItem("user");
+        window.location.replace("index.html");
+        } })*/
