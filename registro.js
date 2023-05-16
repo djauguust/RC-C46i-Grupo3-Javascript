40dec35e62b3f07eba1fb39a24122c42c7084b03
CarlosFadel <carlitosfadel.cdf@gmail.com>
2023-05-16 19:39:22 -0300

    modifico Js de pagina registro, valido, falta alerta para usuarios

--- registro.js
+++ registro.js
@@ -1,124 +1,73 @@
-//clase
-class User {
-  constructor(usuario, email, contraseña) {
-    this.usuario = usuario;
-    this.email = email;
-    this.contraseña = contraseña;
-  }
+let usuarios = [];
+const obtenerusuarios = localStorage.getItem("usuario");
+if (obtenerusuarios) {
+  usuarios = JSON.parse(obtenerusuarios) || [];
 }
 
+console.log(usuarios);
 
-// Accede al formulario de registro
-var registroForm = document.getElementById('registroForm');
+const botonRegistro = document.getElementById("botonRegistrar");
+function saveData() {
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const password2 = document.getElementById("password2").value;
 
-
-
-  // Obtén los valores de los campos
-  const forms = document.querySelectorAll(".needs-validation")
-  const usuario = document.getElementById('User').value;
-  const email = document.getElementById('email').value;
-  const contrasenia = document.getElementById('contraseña').value;
- 
-//agregando eventos
-
-inputUsuarioReg.addEventListener("blur", () => {
-  requiredField(inputUsuarioReg);
+  const usuarioNuevo = {
+    username,
+    email,
+    password,
+  };
+  usuarios.push(usuarioNuevo);
+  localStorage.setItem("usuario", JSON.stringify(usuarios));
+}
+botonRegistro.addEventListener("click", (e) => {
+  const email = document.getElementById("email").value;
+  console.log(emailExistente(email));
+  if (!emailExistente(email) && validarContrasenias(password, password2)) {
+    saveData();
+  }
 });
-
-inputEmailReg.addEventListener("blur", () => {
-  validateEmail(inputEmailReg);
-});
-
-inputPassReg.addEventListener("blur", () => {
-  validatePass(inputPassReg);
-});
-
-formReg.addEventListener("submit", saveUser);
-
-//validaciones
-
-function requiredField(inputUserReg) {
-  if (input.value.trim().length > 0) {
-    input.className = "form-control is-valid";
-    return true;
-  } else {
-    input.className = "form-control is-invalid";
+// funcion para validar email
+function emailExistente(email) {
+  //Validar Email existente
+  const index = usuarios.findIndex((u) => u.email === email);
+  if (index == -1) {
     return false;
+  } else {
+    // Falta mostrar al usuario que el email ya esta registrado
+    return true;
   }
 }
 
-function validateEmail(inputEmailReg) {
-  let regEmail =
-    /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
-  if (regEmail.test(input.value)) {
-    input.className = "form-control is-valid";
-    return true;
-  } else {
-    input.className = "form-control is-invalid";
-    return false;
-  }
+function validarContrasenias(c1, c2) {
+  return c1 === c2;
 }
 
-function validatePass(inputPassReg) {
-  let regPass = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/;
-  if (regPass.test(input.value)) {
-    input.className = "form-control is-valid";
-    return true;
-  } else {
-    input.className = "form-control is-invalid";
-    return false;
-  }
-}
-
-function gralValidate(inputUser, inputEmail, inputPass) {
+// Función para validar el formulario de registro
+function validarRegistro(username, email, password, password2) {
   if (
-    requiredField(inputUser) &&
-    validateEmail(inputEmail) &&
-    validatePass(inputPass)
+    email != null &&
+    email != "" &&
+    password != null &&
+    password != "" &&
+    password2 != null &&
+    password2 != "" &&
+    username != null &&
+    username != ""
   ) {
+   //si pasa las validaciones
     return true;
-  } else {
-    return false;
   }
-};
 
-function saveUser(e) {
-  e.preventDefault();
-  if (gralValidate(inputUserReg, inputEmailReg, inputPassReg)) {
-    createUser();
-    window.setTimeout(function () {
-      window.location.replace("login.html");
-    }, 1500);
-  } else {
-    alert("Debe completar todos los campos");
+  //Falta mostrar al usuario que debe cmpletar todos los campos
+  return false;
+}
+//Asignar el evento submit al formulario
+document.getElementById("usuario").addEventListener("click", function (e) {
+  e.preventDefault(); // Evitar envío del formulario si no pasa las validaciones
+  if (validarRegistro()) {
+    // Aquí puedes enviar el formulario o realizar otras acciones necesarias
+    alert("Formulario válido. Se puede realizar el registro.");
   }
-}
-
-function createUser() {
-  let newUser = new User(
-    inputUserReg.value,
-    inputEmailReg.value,
-    inputPassReg.value
-  );
-  regUser.push(newUser);
-  cleanFormUser();
-  Swal.fire(
-    "Usuario creado",
-    "Su usuario fue correctamente cargado",
-    "success"
-  );
-}
-
-function cleanFormUser() {
-  formReg.reset();
-  inputUsuarioReg.className = "form-control";
-  inputEmailReg.className = "form-control";
-  inputPassReg.className = "form-control";
-  saveUserLS();
-}
-
-function saveUserLS() {
-  localStorage.setItem("regUser", JSON.stringify(regUser));
-};
-
-console.log(inputUsuarioReg);
+});
